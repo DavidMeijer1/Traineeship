de285cd55c79535b57fa08ed48cc8361f85182a3
DavidMeijer1 <126792109+DavidMeijer1@users.noreply.github.com>
2023-06-25 13:19:00 +0200

    Delete src/playersGuide/Day18 directory

--- src/playersGuide/Day18/Program.java
+++
@@ -1,8 +0,0 @@
-package playersGuide.Day18;
-
-public class Program {
-    public static void main(String[] args) {
-        ManticoreHunting manticoreHunting = new ManticoreHunting();
-        manticoreHunting.runGame();
-    }
-}
