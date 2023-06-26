fe955b4b6a4829b7502fdbfc717618b1a1077188
David <davidmeijer94@gmail.com>
2023-06-26 13:58:55 +0200

    Commit message

--- src/playersGuide/day25/CardColours.java
+++ src/playersGuide/day25/CardColours.java
@@ -1,5 +1,15 @@
 package playersGuide.day25;
 
 public enum CardColours {
-    RED, GREEN, BLUE, YELLOW;
+    RED("Red"), GREEN("Green"), BLUE("Blue"), YELLOW("Yellow");
+
+    public String getColour() {
+        return colour;
+    }
+
+    String colour;
+
+    CardColours(String colour) {
+        this.colour = colour;
+    }
 }
